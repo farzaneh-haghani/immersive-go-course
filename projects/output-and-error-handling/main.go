b85b0ed51fc97404ffa0ba546fda54d802dcbccc
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-04-27 15:17:46 +0100

    function handleStatusCode added
    To solve the problem with defer

--- projects/output-and-error-handling/main.go
+++ projects/output-and-error-handling/main.go
@@ -16,10 +16,45 @@ func sendRequest(url string) *http.Response {
 		os.Exit(2)
 	}
 	if resp == nil {
-		fmt.Fprint(os.Stderr, "The server did not reply anything, which here is considered an error!")
+		fmt.Fprintln(os.Stderr, "The server did not reply anything, which here is considered an error!")
 		os.Exit(52)
 	}
 	return resp
+}
+
+func handleStatusCode(resp *http.Response, sleepTime *time.Duration) {
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		scanner := bufio.NewScanner(resp.Body)
+		for i := 0; scanner.Scan(); i++ {
+			fmt.Println(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+		os.Exit(0)
+	} else if resp.StatusCode == 429 {
+		// If I receive 429 but can't find ["Retry-After"] so I determine 1s to sleep. It act same as a while
+		retryAfter := resp.Header.Get("Retry-After")
+		if retryAfter == "" || retryAfter == "a while" {
+			// I added with the previous sleep times to prevent sleeping for too long and more than 5s give up.
+			*sleepTime += time.Second
+		} else {
+
+			*sleepTime = parseRetryAfterNumber(retryAfter)
+		}
+		if *sleepTime > 5*time.Second {
+			fmt.Println("We can't get the weather!")
+			fmt.Fprintln(os.Stderr, "Sleep time is more than 5 second so HTTP page not retrieved. Returned another error with the HTTP error code being 400 or above.")
+			os.Exit(22)
+		} else if *sleepTime > time.Second {
+			fmt.Println("Things may be a bit slow because we're doing a retry!")
+		}
+		time.Sleep(*sleepTime)
+	} else {
+		fmt.Fprintln(os.Stderr, "Failed to initialize!")
+		os.Exit(2)
+	}
 }
 
 func parseRetryAfterNumber(retryAfter string) time.Duration {
@@ -34,6 +69,8 @@ func parseRetryAfterString(retryAfter string) time.Duration {
 	sleepTime := time.Second
 	if timeStamp, err := time.Parse(http.TimeFormat, retryAfter); err == nil {
 		sleepTime = time.Until(timeStamp)
+	} else {
+		fmt.Fprintln(os.Stderr, "There was an error in the value of Retry-After", err)
 	}
 	return sleepTime
 }
@@ -42,47 +79,15 @@ func main() {
 	var sleepTime time.Duration
 	for {
 		resp := sendRequest("http://localhost:8080")
-		if resp.StatusCode == 200 {
-			scanner := bufio.NewScanner(resp.Body)
-			for i := 0; scanner.Scan(); i++ {
-				fmt.Println(scanner.Text())
-			}
-			if err := scanner.Err(); err != nil {
-				panic(err)
-			}
-			resp.Body.Close()
-			os.Exit(0)
-		} else if resp.StatusCode == 429 {
-			// If I receive 429 but can't find ["Retry-After"] so I determine 5s to sleep
-			// because you mentioned more than 5s give up.
-			if resp.Header["Retry-After"] == nil {
-				// I added with the previous sleep times to prevent sleeping for too long if all previous ones had sleep time.
-				sleepTime += 5 * time.Second
-			} else {
-				retryAfter := resp.Header.Get("Retry-After")
-				sleepTime += parseRetryAfterNumber(retryAfter)
-			}
-			if sleepTime > 5*time.Second {
-				fmt.Println("We can't get the weather!")
-				resp.Body.Close()
-				fmt.Fprintln(os.Stderr, "Sleep time is more than 5 second so HTTP page not retrieved. Returned another error with the HTTP error code being 400 or above.")
-				os.Exit(22)
-			} else if sleepTime > time.Second {
-				fmt.Println("Things may be a bit slow because we're doing a retry!")
-			}
-			time.Sleep(sleepTime)
-		} else {
-			resp.Body.Close()
-			fmt.Fprintln(os.Stderr, "Failed to initialize!")
-			os.Exit(2)
-		}
+		handleStatusCode(resp, &sleepTime)
 	}
 }
 
 // strconv.Atoi(str)                                 To convert a string representing an int number to int type
 // strconv.ParseFloat(str, 64)                       To convert a string representing a float 64 number to float 64 type
-// time.Duration(float64(time.Second) * sleepTime)   To convert float 64 to time.Duration
+// time.Duration(number) * time.Second               To convert a number to time.Duration in second - without multiply it's in nanosecond
 // time.Parse(http.TimeFormat, timeStr)              To convert time stamp (Thu, 25 Apr 2024 16:05:40 GMT ) to time.Time type (2024-04-25 16:05:40 +0000 UTC)
+// time.ParseDuration("durationStr")                 To convert a string representing a duration to time.Duration
 // time.Now().Add(time.Second)                       To add one second to current time with time.Time type
 // time.Second                                       To return 1s with time.Duration type - we can multiple it by number of second we want
 // endTime.Sub(startTime)                            To get time.Duration type from two times with time.Time type
