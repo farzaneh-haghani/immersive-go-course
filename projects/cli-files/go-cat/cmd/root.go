d35f5cb0e426600fecb07346f870647588b3cdb8
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-05-09 20:41:44 +0100

    multiple arguments & -n handled

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -8,29 +8,51 @@ import (
 
 func Execute() error {
 	pathArr := os.Args[1:]
+
 	if len(pathArr) == 0 {
 		return fmt.Errorf("did not provide any path")
 	}
-	path := pathArr[0]
-	isValid, err := os.Stat(path)
-	if err != nil {
-		return fmt.Errorf("%s: no such file or directory", path)
+
+	var i int
+	if pathArr[0] == "-n" {
+		i = 1
+	} else {
+		i = 0
 	}
-	directory := isValid.IsDir()
-	if directory {
-		return fmt.Errorf("%s: is a directory", path)
-	}
-	file, err := os.Open(path)
-	if err != nil {
-		return fmt.Errorf("%s: is not readable", path)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for i := 0; scanner.Scan(); i++ {
-		os.Stdout.Write([]byte(scanner.Text()+"\n"))
-	}
-	if err := scanner.Err(); err != nil {
-		return err
+
+	for i < len(pathArr) {
+		path := pathArr[i]
+
+		isPathValid, err := os.Stat(path)
+		if err != nil {
+			return fmt.Errorf("%s: no such file or directory", path)
+		}
+
+		isDirectory := isPathValid.IsDir()
+		if isDirectory {
+			return fmt.Errorf("%s: is a directory", path)
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("%s: is not readable", path)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for i := 0; scanner.Scan(); i++ {
+			if pathArr[0] == "-n" {
+				text := fmt.Sprint("     ", i+1, "  ", scanner.Text()+"\n")
+				os.Stdout.Write([]byte(text))
+			} else {
+				os.Stdout.Write([]byte(scanner.Text() + "\n"))
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			return err
+		}
+		i++
 	}
 	return nil
 }
