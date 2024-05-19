63e7745b0395fd40118a13b3d08d806d384e18a4
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-05-19 20:59:09 +0100

    refactoring for cat

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -2,57 +2,78 @@ package cmd
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func Execute() error {
-	pathArr := os.Args[1:]
 
-	if len(pathArr) == 0 {
+	numberLine := flag.Bool("n", false, "Add number in front of each line")
+	help := flag.Bool("h", false, "Show help")
+	helpFilePath := "assets/help.txt"
+
+	flag.Parse()
+
+	var pathArr []string
+
+	if *numberLine && *help {
+		pathArr = os.Args[3:]
+	} else if !*numberLine && !*help {
+		pathArr = os.Args[1:]
+	} else {
+		pathArr = os.Args[2:]
+	}
+
+	if *help {
+		err := openFile(helpFilePath, false)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(pathArr) == 0 && !*help {
 		return fmt.Errorf("did not provide any path")
 	}
 
-	var i int
-	if pathArr[0] == "-n" {
-		i = 1
-	} else {
-		i = 0
-	}
-
-	for i < len(pathArr) {
-		path := pathArr[i]
-
-		isPathValid, err := os.Stat(path)
+	for _, path := range pathArr {
+		err := openFile(path, *numberLine)
 		if err != nil {
-			return fmt.Errorf("%s: no such file or directory", path)
-		}
-
-		isDirectory := isPathValid.IsDir()
-		if isDirectory {
-			return fmt.Errorf("%s: is a directory", path)
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return fmt.Errorf("%s: is not readable", path)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for i := 0; scanner.Scan(); i++ {
-			if pathArr[0] == "-n" {
-				text := fmt.Sprint("     ", i+1, "  ", scanner.Text()+"\n")
-				os.Stdout.Write([]byte(text))
-			} else {
-				os.Stdout.Write([]byte(scanner.Text() + "\n"))
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
 			return err
 		}
-		i++
+	}
+	return nil
+}
+
+func openFile(path string, numberLine bool) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	isPathValid, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	isDirectory := isPathValid.IsDir()
+	if isDirectory {
+		return fmt.Errorf("%s: is a directory", path)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for i := 0; scanner.Scan(); i++ {
+		if numberLine {
+			text := fmt.Sprint("     ", i+1, "  ", scanner.Text()+"\n")
+			os.Stdout.Write([]byte(text))
+		} else {
+			os.Stdout.Write([]byte(scanner.Text() + "\n"))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
 	}
 	return nil
 }
