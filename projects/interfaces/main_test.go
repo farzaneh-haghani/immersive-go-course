578445b35fa692320754cd2582adf074f5e85883
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-06-21 14:53:35 +0100

    struct & funcs implemented

--- projects/interfaces/main_test.go
+++ projects/interfaces/main_test.go
@@ -6,26 +6,25 @@ import (
 )
 
 func TestBuffer(t *testing.T) {
-	t.Run("If I make a buffer named b containing some bytes, calling b.Bytes() returns the same bytes I created it with.", func(t *testing.T) {
+	t.Run("Make OurByteBuffer buffer named b containing some bytes, calling b.Bytes() returns the same bytes I created it with.", func(t *testing.T) {
 		text := "Hello"
-		var b bytes.Buffer
-		b.WriteString(text)
-		got := b.Bytes()
+		var b OurByteBuffer
+		b.OurWriteString(text)
+		got := b.OurBytes()
 		want := []byte(text)
 
-		// if string(got)!=string(want){
 		if !bytes.Equal(got, want) {
 			t.Errorf("got %s but expect %s", got, want)
 		}
 	})
 
-	t.Run("If I write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.", func(t *testing.T) {
+	t.Run("Write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.", func(t *testing.T) {
 		text1 := "Hello"
 		text2 := " world"
-		var b bytes.Buffer
-		b.WriteString(text1)
-		b.WriteString(text2)
-		got := b.Bytes()
+		var b OurByteBuffer
+		b.OurWriteString(text1)
+		b.OurWriteString(text2)
+		got := b.OurBytes()
 		want := []byte(text1 + text2)
 
 		if !bytes.Equal(got, want) {
@@ -35,25 +34,24 @@ func TestBuffer(t *testing.T) {
 
 	t.Run("If I call b.Read() with a slice big enough to read all of the bytes in the buffer, all of the bytes are read.", func(t *testing.T) {
 		text := "Hello world"
-		var b bytes.Buffer
-		b.WriteString(text)
+		var b OurByteBuffer
+		b.OurWriteString(text)
 		s := make([]byte, len(text))
-		b.Read(s)
+		b.OurRead(s)
 		want := []byte(text)
 
 		if !bytes.Equal(s, want) {
 			t.Errorf("got %s but expect %s", s, want)
 		}
-
 	})
 
-	t.Run("If I call b.Read() with a slice smaller than the contents of the buffer, some of the bytes are read. If I call it again, the next bytes are read.", func(t *testing.T) {
+	t.Run("Call b.Read() with a slice smaller than the contents of the buffer, some of the bytes are read. If I call it again, the next bytes are read.", func(t *testing.T) {
 		text := "Hello world"
-		var b bytes.Buffer
-		b.WriteString(text)
-		s := make([]byte, 6)
-		b.Read(s)
-		b.Read(s)
+		var b OurByteBuffer
+		b.OurWriteString(text)
+		s := make([]byte, len(text)-5)
+		b.OurRead(s)
+		b.OurRead(s)
 		want := []byte("world ")
 
 		if !bytes.Equal(s, want) {
