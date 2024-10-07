2d5829fd0f7012a3904ecced5db469971370afb7
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-10-07 03:21:59 +0100

    static changed to struct & rwmutex added

--- projects/concurrency/main.go
+++ projects/concurrency/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Node[K comparable, V any] struct {
 	key         K
@@ -16,14 +19,19 @@ type List[K comparable, V any] struct {
 	last  *Node[K, V]
 }
 
-type Cache[K comparable, V any] struct {
-	size             int
-	m                map[K]*Node[K, V]
+type static struct {
 	readCount        int //Question 1
 	unreadCount      int //Question 1
 	entriesNeverRead int //Question 2
 	totalReadExisted int //Question 3
 	writesCount      int //Question 4
+}
+type Cache[K comparable, V any] struct {
+	size int
+	m    map[K]*Node[K, V]
+	l    List[K, V]
+	s    static
+	rwm  sync.RWMutex
 }
 
 func NewNode[K comparable, V any](key K, value V) *Node[K, V] {
@@ -44,17 +52,24 @@ func NewList[K comparable, V any]() *List[K, V] {
 	}
 }
 
-func NewCache[K comparable, V any](entryLimit int) (Cache[K, V], *List[K, V]) { //All K should be unique && same type && comparable type like primitive that can compare with each other
-	list := NewList[K, V]()
-	return Cache[K, V]{
-		size:             entryLimit,
-		m:                make(map[K]*Node[K, V]),
+func NewStatic() *static {
+	return &static{
 		readCount:        0,
 		unreadCount:      0,
 		entriesNeverRead: 0,
 		totalReadExisted: 0,
 		writesCount:      0,
-	}, list
+	}
+}
+
+func NewCache[K comparable, V any](entryLimit int) Cache[K, V] { //All K should be unique && same type && comparable type like primitive that can compare with each other
+
+	return Cache[K, V]{
+		size: entryLimit,
+		m:    make(map[K]*Node[K, V]),
+		l:    *NewList[K, V](),
+		s:    *NewStatic(),
+	}
 }
 
 func (l *List[K, V]) addNode(key K, value V) *Node[K, V] {
@@ -100,59 +115,95 @@ func (l *List[K, V]) deleteNode() (K, int) {
 	return deleted, entriesRead
 }
 
-func (c *Cache[K, V]) Put(key K, l *List[K, V], value V) bool {
+func (c *Cache[K, V]) Put(key K, value V) bool {
+	c.rwm.Lock()
 	if currentNode, isExisted := c.m[key]; isExisted {
 		currentNode.value = value
-		l.moveNode(currentNode)
+		c.l.moveNode(currentNode)
+		c.rwm.Unlock()
 		return true
 	} else if len(c.m) >= c.size {
-		deleted, entriesRead := l.deleteNode()
+		deleted, entriesRead := c.l.deleteNode()
 		delete(c.m, deleted)
-		c.totalReadExisted -= entriesRead
+		c.s.totalReadExisted -= entriesRead
 	}
-	newNode := l.addNode(key, value)
+	newNode := c.l.addNode(key, value)
 	c.m[key] = newNode
-	c.entriesNeverRead++
-	c.writesCount++
+	c.s.entriesNeverRead++
+	c.s.writesCount++
+	c.rwm.Unlock()
 	return false
 }
 
-func (c *Cache[K, V]) Get(key K, l *List[K, V]) (*Node[K, V], bool) {
+func (c *Cache[K, V]) Get(key K) (*V, bool) {
+	c.rwm.RLock()
 	if currentNode, isExisted := c.m[key]; isExisted {
-		l.moveNode(currentNode)
-		c.readCount++
-		c.totalReadExisted++
+		c.l.moveNode(currentNode)
+		c.s.readCount++
+		c.s.totalReadExisted++
 		currentNode.entriesRead++
 		if currentNode.isRead == false {
 			currentNode.isRead = true
-			c.entriesNeverRead--
+			c.s.entriesNeverRead--
 		}
-		return currentNode, true
+		c.rwm.RUnlock()
+		return &currentNode.value, true
 	}
-	c.unreadCount++
+	c.s.unreadCount++
+	c.rwm.RUnlock()
 	return nil, false
 }
 
+var wg sync.WaitGroup
+
 func main() {
-	cache, list := NewCache[int, string](4)
-	cache.Put(1, list, "newValue1")
-	cache.Put(2, list, "newValue2")
-	cache.Put(3, list, "newValue3")
-	cache.Get(2, list)
-	cache.Put(4, list, "newValue4")
-	cache.Put(2, list, "newValue22")
-	cache.Put(5, list, "newValue5")
-	if value, isExisted := cache.Get(2, list); isExisted {
-		fmt.Printf("Value is:%s\n", value.value)
-	} else {
-		fmt.Println("Value isn't exist\n")
-	}
-	if totalHit := cache.readCount + cache.unreadCount; totalHit == 0 {
+	cache := NewCache[int, string](4)
+	wg.Add(7)
+	go func() {
+		cache.Put(1, "newValue1")
+		wg.Done()
+	}()
+	go func() {
+		cache.Put(2, "newValue2")
+		wg.Done()
+	}()
+	go func() {
+		cache.Put(3, "newValue3")
+		wg.Done()
+	}()
+	go func() {
+		cache.Get(2)
+		wg.Done()
+	}()
+	go func() {
+		cache.Put(4, "newValue4")
+		wg.Done()
+	}()
+	go func() {
+		cache.Put(2, "newValue22")
+		wg.Done()
+	}()
+	go func() {
+		cache.Put(5, "newValue5")
+		wg.Done()
+	}()
+	wg.Wait()
+	wg.Add(1)
+	go func() {
+		if value, isExisted := cache.Get(2); isExisted {
+			fmt.Printf("Value is:%v\n", *value)
+		} else {
+			fmt.Println("Value isn't exist")
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	if totalHit := cache.s.readCount + cache.s.unreadCount; totalHit == 0 {
 		fmt.Println("Never have had any read from this cache!")
 	} else {
-		fmt.Printf("Hit rate:%d\n", cache.readCount/totalHit*100)
+		fmt.Printf("Hit rate:%.2f\n", float64(cache.s.readCount)/float64(totalHit)*100)
 	}
-	fmt.Printf("Entries were written to the cache and have never been read: %d\n", cache.entriesNeverRead)
-	fmt.Printf("Average number of times that things currently in the cache is read: %.2f\n", float64(cache.totalReadExisted)/float64(len(cache.m)))
-	fmt.Printf("Total reads and writes have been performed in the cache including evicted: %d\n", cache.readCount+cache.writesCount)
+	fmt.Printf("Entries were written to the cache and have never been read: %d\n", cache.s.entriesNeverRead)
+	fmt.Printf("Average number of times that things currently in the cache is read: %.2f\n", float64(cache.s.totalReadExisted)/float64(len(cache.m)))
+	fmt.Printf("Total reads and writes have been performed in the cache including evicted: %d\n", cache.s.readCount+cache.s.writesCount)
 }
