a6ab612069120952791783e21f34bd50483dbb76
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-11-11 16:02:28 +0000

    handle exceptional situation
    situation of replacing cache cell by the next value before pending go routines can read it (cache with one entry limit)

--- projects/concurrency/computing-cache/computing-cache.go
+++ projects/concurrency/computing-cache/computing-cache.go
@@ -3,23 +3,22 @@ package computingCache
 import (
 	"concurrency/cache"
 	"sync"
-	"time"
 )
 
 type computingCache[K comparable, V any] struct {
 	cache           cache.Cache[K, V]
 	Creator         func(K) V
-	pendingRequests map[K]time.Time
-	responseTime    time.Duration
-	mu              sync.Mutex
+	pendingRequests map[K][]chan V // My brain didn't work more so I cheated the idea of slice channel from your sample solutions
+	// responseTime    time.Duration
+	mu sync.Mutex
 }
 
 func NewComputingCache[K comparable, V any](entryLimit int, Creator func(K) V) computingCache[K, V] {
 	return computingCache[K, V]{
 		cache:           cache.NewCache[K, V](entryLimit),
 		Creator:         Creator,
-		pendingRequests: map[K]time.Time{},
-		responseTime:    0,
+		pendingRequests: make(map[K][]chan V),
+		// responseTime:    0,
 	}
 }
 
@@ -28,40 +27,28 @@ func Creator[K comparable](key int) string {
 	return value
 }
 
-func (c *computingCache[K, V]) SendRequest(key K) V {
-	c.mu.Lock()
-	c.pendingRequests[key] = time.Now()
-	c.mu.Unlock()
-	value := c.Creator(key)
-	c.cache.Put(key, value)
-	c.mu.Lock()
-	delete(c.pendingRequests, key)
-	c.mu.Unlock()
-	return value
-}
-
 func (c *computingCache[K, V]) Get(key K) V {
 	if currentValue, isExisted := c.cache.Get(key); isExisted {
 		return *currentValue
 	}
 	c.mu.Lock()
-	if requestedTime, isExisted := c.pendingRequests[key]; isExisted {
+	if channels, isExisted := c.pendingRequests[key]; isExisted {
+		newChannel := make(chan V)
+		c.pendingRequests[key] = append(channels, newChannel)
 		c.mu.Unlock()
-		for {
-			if time.Since(requestedTime) > c.responseTime+1000000000 {
-				value := c.SendRequest(key)
-				return value
-			}
-			time.Sleep(time.Since(requestedTime) + 1000000000)
-			if value, isExisted := c.cache.Get(key); isExisted {
-				c.mu.Lock()
-				delete(c.pendingRequests, key)
-				c.mu.Unlock()
-				return *value
-			}
-		}
+
+		value := <-newChannel //If the first go routine after writing in the map, lose connection, all the rest go routines will stay forever?
+		return value
 	}
+	c.pendingRequests[key] = []chan V{}
 	c.mu.Unlock()
-	value := c.SendRequest(key)
+	value := c.Creator(key)
+	c.cache.Put(key, value)
+	c.mu.Lock()
+	for _, channel := range c.pendingRequests[key] {
+		channel <- value
+	}
+	delete(c.pendingRequests, key)
+	c.mu.Unlock()
 	return value
 }
